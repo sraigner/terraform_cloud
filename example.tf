5b2d412caddc4b05280f01839eb573510c02fae9
scottraignerR <scott.raigner@realwear.com>
2019-10-02 09:30:25 -0700

    Changes for next go around

--- example.tf
+++ example.tf
@@ -6,3 +6,26 @@ terraform {
           }
   }
 }
+
+   provider "aws" {
+	profile	= "default"
+	region	= var.region
+   }
+
+   resource "aws_instance" example {
+   	ami	= var.amis[var.region]
+	instance_type = "t2.micro"
+
+	provisioner "local-exec" {
+	   command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+           }
+   }
+
+   resource "aws_eip" "ip2" {
+	vpc = true
+	instance = aws_instance.example.id
+   }
+
+   output "ip_out" {
+        value = aws_eip.ip2.public_ip
+   }
